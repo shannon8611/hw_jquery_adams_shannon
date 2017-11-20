b5e0344b7d338fb3b4bfb1f571c4eed6f28cfe72
shannon8611 <shannon8611@yahoo.com>
2017-11-19 19:21:13 -0600

    add js code for paragrpahs

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+    var paragraphs = $('p');
+
+    paragraphs.css({
+        'color': '#7cb2b2',
+        'font-weight': 'bold',
+        'background-color': '#515e5e'
+    });
+});
