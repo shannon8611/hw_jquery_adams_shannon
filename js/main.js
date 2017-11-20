6b55cffde2c119f211eb6d44e6c73756a7e686f6
shannon8611 <shannon8611@yahoo.com>
2017-11-19 20:05:04 -0600

    create empty jquery ready function

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
 $(document).ready(function () {
 
-    var paragraphs = $('p');
-
-    paragraphs.css({
-        'color': '#7cb2b2',
-        'font-weight': 'bold',
-        'background-color': '#515e5e'
-    });
 });
